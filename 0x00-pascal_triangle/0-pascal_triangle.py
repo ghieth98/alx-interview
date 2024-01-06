7ca05cb73ab46bcbe02c51bd9230649080e4fefc
hossam ghieth <hossamghieth@gmail.com>
2024-01-06 18:45:16 +0200

    Create 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+Pascal Triangle Tree
+"""
+
+
+def pascal_triangle(n):
+    """
+    Method to draw a pascal triangle throw given an integer n
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        arr = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                arr[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(arr)
+
+    return triangle
