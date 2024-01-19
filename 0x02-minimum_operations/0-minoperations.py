be964fdeedb5fe05c25f4fac7ac2373e039cff51
hossam ghieth <hossamghieth@gmail.com>
2024-01-19 04:05:44 +0200

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -79,7 +79,7 @@ def minOperations_v3(n: int) -> int:
     for i in range(2, n + 1):
         dp[i] = float('inf')
 
-        for j in range(1, int(i**0.5) + 1):
+        for j in range(1, int(i ** 0.5) + 1):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + i // j, dp[i // j] + j)
 
