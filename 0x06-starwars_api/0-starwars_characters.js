825ac7966fe1386598dd0c4c95b359f7287c30e6
ghieth98 <hossamghieth@gmail.com>
2024-02-16 00:42:43 +0200

    update file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,46 +1,41 @@
 #!/usr/bin/node
-const request = require('request');
+/* a script that prints all characters of a Star Wars movie:
+*/
+const util = require('util');
+const request = util.promisify(require('request'));
+const id = process.argv[2];
+const apiURL = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-function fetch(url) {
-    return new Promise((resolve, reject) => {
-        request.get(url, (err, response, body) => {
-            if (err) {
-                reject(err);
-            } else if (response.statusCode === 200) {
-                resolve(body);
-            } else {
-                reject(new Error(`Request failed. Status code: ${response.statusCode}`));
+function getUsers(urls, ids) {
+    request(urls[ids], (err, res, body) => {
+        if (!err) {
+            console.log(JSON.parse(body).name);
+            if (ids < urls.length - 1) {
+                getUsers(urls, ids + 1);
             }
+        }
+    });
+}
+
+// Parallel Exectution Version
+function fetchUsers(urls) {
+    const fetchPromises = urls.map((url) => request({url}));
+
+    Promise.all(fetchPromises)
+        .then((responses) => {
+            responses.forEach((resp) => console.log(JSON.parse(resp.body).name));
+        })
+        .catch((err) => {
+            console.error(err);
         });
-    });
 }
 
-function fetchCharactersRecursively(charData, index) {
-    if (index >= charData.length) {
-        return;
+request(apiURL, (err, res, body) => {
+    if (!err) {
+        const allCharacters = JSON.parse(body).characters;
+        fetchUsers(allCharacters);
+        if (allCharacters.length < 0) {
+            getUsers(allCharacters, 0);
+        }
     }
-
-    const characterUrl = charData[index];
-
-    fetch(characterUrl).then(body => {
-        const character = JSON.parse(body);
-        console.log(character.name);
-        fetchCharactersRecursively(charData, index + 1);
-    }).catch(err => {
-        console.error(err);
-        fetchCharactersRecursively(charData, index + 1);
-    });
-}
-
-function fetchMovieCharacters(movieID) {
-    const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
-
-    fetch(apiUrl).then(body => {
-        const movieData = JSON.parse(body);
-        const charData = movieData.characters;
-        fetchCharactersRecursively(charData, 0);
-    }).catch(err => console.error(err));
-}
-
-const movieID = process.argv[2];
-fetchMovieCharacters(movieID);
+});
