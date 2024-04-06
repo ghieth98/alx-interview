767f3c2c594b3308e3b9fa8761e96f2c486d5842
hossam ghieth <hossamghieth@gmail.com>
2024-04-06 20:50:55 +0200

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,24 +2,26 @@
 /* a script that prints all characters of a Star Wars movie:
 */
 const util = require('util');
+// const request = require('request');
 const request = util.promisify(require('request'));
 const id = process.argv[2];
 const apiURL = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-function getUsers(urls, ids) {
-    request(urls[ids], (err, res, body) => {
+// Serial Execution Version
+function getXters (urls, idx) {
+    request(urls[idx], (err, res, body) => {
         if (!err) {
             console.log(JSON.parse(body).name);
-            if (ids < urls.length - 1) {
-                getUsers(urls, ids + 1);
+            if (idx < urls.length - 1) {
+                getXters(urls, idx + 1);
             }
         }
     });
 }
 
 // Parallel Exectution Version
-function fetchUsers(urls) {
-    const fetchPromises = urls.map((url) => request({url}));
+function fetchXters (urls) {
+    const fetchPromises = urls.map((url) => request({ url }));
 
     Promise.all(fetchPromises)
         .then((responses) => {
@@ -33,9 +35,9 @@ function fetchUsers(urls) {
 request(apiURL, (err, res, body) => {
     if (!err) {
         const allCharacters = JSON.parse(body).characters;
-        fetchUsers(allCharacters);
+        fetchXters(allCharacters);
         if (allCharacters.length < 0) {
-            getUsers(allCharacters, 0);
+            getXters(allCharacters, 0);
         }
     }
 });
